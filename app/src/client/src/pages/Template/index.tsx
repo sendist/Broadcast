9e640638dae1320e7c3fe7328677ffa51693d4dc
sendist <sendisetiawan10902@gmail.com>
2023-10-01 09:57:09 +0700

    implement template feature

--- app/src/client/src/pages/Template/index.tsx
+++ app/src/client/src/pages/Template/index.tsx
@@ -1,3 +1,44 @@
+import { DataTable } from "@/components/ui/data-table";
+import { Template, columns } from "./columns";
+import { useCRUD } from "@/hooks/backend";
+import { AddTemplateForm } from "./add";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
+import { useApiFetch } from "@/hooks/fetch";
+import { BASE_URL } from "@/lib/constants";
+
 export default function Template() {
-  return <div>ini template</div>;
+  const { data, loading, update, remove, create } = useCRUD<Template>({
+    url: "/template",
+  });
+
+  const apiFetch = useApiFetch();
+
+  return (
+    <div>
+      <div className="space-x-4">
+        <AddTemplateForm onSubmit={create}>
+          <Button variant="outline" className="mb-4">
+            <PlusIcon className="mr-2" />
+            Add
+          </Button>
+        </AddTemplateForm>
+      </div>
+      <DataTable
+        columns={columns}
+        data={data}
+        isLoading={loading}
+        meta={{
+          updateData: (id: string, key: string, value: unknown) => {
+            update(id, {
+              [key]: value,
+            });
+          },
+          removeData: (id: string) => {
+            remove(id);
+          },
+        }}
+      />
+    </div>
+  );
 }
