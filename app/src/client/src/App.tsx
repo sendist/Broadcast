0d99a1624a41169f46af6167fe3b7661b5f57b81
sendist <sendisetiawan10902@gmail.com>
2023-10-08 15:06:48 +0700

    add logo

--- app/src/client/src/App.tsx
+++ app/src/client/src/App.tsx
@@ -199,7 +199,26 @@ function App() {
         >
           <div className="flex h-full flex-col overflow-y-auto border-r border-slate-200 bg-white px-3 py-4">
             <div className="mb-10 flex items-center rounded-lg px-3 py-2 text-slate-900">
-              <span className="ml-3 text-base font-semibold">Broadcast</span>
+              <svg
+                className="h-9 w-9"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0.2999999999999998 89 102.1"
+                width="50.4"
+                height="58.1643594"
+                x="4.800000000000001"
+                y="5.539463443183575"
+                fill="#557A82"
+              >
+                <path
+                  d="M24.6 89V57.7c0-11 19.9-19.9 19.9-19.9s19.9 8.9 19.9 19.9V89m-.1-16.2v-28c0-11-19.9-19.9-19.9-19.9s-19.9 8.9-19.9 19.9v28m39.8-11.1v-28c0-11-19.9-19.9-19.9-19.9s-19.9 8.9-19.9 19.9v28M2 75.9l42.5 24.5L87 75.9V26.8L44.5 2.3 2 26.8v49.1zm73.9-55.4v61.8M13.1 20.5v61.8"
+                  fill="none"
+                  stroke="#557A82"
+                  stroke-width="4px"
+                  stroke-miterlimit="10"
+                ></path>
+              </svg>
+              <span className="ml-1 text-base font-semibold">Broadcast</span>
             </div>
             <ul className="space-y-2 text-sm font-medium">
               {menus.map((menu, index) => (
