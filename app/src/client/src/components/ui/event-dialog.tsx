eb90033dc54c91fc7384ceb709621432b3b9201b
meymeiziagaltiady <meymeizia@gmail.com>
2023-11-07 18:47:14 +0700

    add component event calendar

---
+++ app/src/client/src/components/ui/event-dialog.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./dialog";
+import EventDetail from "./event-detail";
+import { formatDate } from "@/lib/utils";
+
+type EventData = {
+  jadwal: string;
+  masjid: string;
+  mubaligh: string;
+  waktu?: string;
+};
+
+type Props = {
+  children: React.ReactNode;
+  tanggal: Date;
+  eventData: EventData[];
+};
+
+export default function EventDialog({ children, tanggal, eventData }: Props) {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  return (
+    <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+      <DialogTrigger asChild onClick={() => setDialogOpen(true)}>
+        {children}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{formatDate(tanggal)}</DialogTitle>
+        </DialogHeader>
+        {eventData.map((event)=>(
+          <EventDetail jadwal={event.jadwal} masjid={event.masjid} mubaligh={event.mubaligh} waktu={event.waktu} />
+        ))}
+      </DialogContent>
+    </Dialog>
+  );
+}
