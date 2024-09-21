de57055f92acbe77e0c69ceb087f03a62c844e4e
nayarasaffa <nayara.saffa.tif421@polban.ac.id>
2024-09-21 11:48:34 +0700

    Add search bar component

---
+++ app/src/client/src/components/ui/search-bar.tsx
@@ -0,0 +1,26 @@
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+
+export function SearchBar({
+  value,
+  onChange,
+  placeholder,
+}: {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder: string;
+}) {
+  return (
+    <div className="relative w-full">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <SearchIcon className="w-4 h-4 text-gray-400" />
+      </div>
+      <Input
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="pl-10"
+      />
+    </div>
+  );
+}
